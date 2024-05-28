cc6a9935dc1d128431b842eba606b4a9992e0ba8
AdrianaC27 <adrianacristinasg27@gmail.com>
2024-05-28 11:56:05 -0300

    commit

--- src/Screens/TaskList.jsx
+++ src/Screens/TaskList.jsx
@@ -2,26 +2,36 @@ import React from "react";
 import { SafeAreaView, Text, View ,ImageBackground ,StyleSheet } from "react-native";
 
 import todayImage from '../../assets/imgs/today.jpg';
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import commonStyles from "../commonStyles";
+
+import Task from "../components/Task";
 
 export default props =>{
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
     return (
     <SafeAreaView style ={style.container}>
-        <ImageBackground source ={todayImage} style={style.ImageBackground}>      
+        <ImageBackground source ={todayImage} style={style.ImageBackground}>  
+        <View style ={ style.titleBar}>
+            <Text style ={style.title}>Hoje</Text>
+            <Text style ={style.subTitle}>{today}</Text>
+            
+        </View>    
 
         </ImageBackground>
 
         <View style ={ style.tasklist}>
-        <Text > Task list</Text>
+        <Task>
+            
+        </Task>
 
         </View>
-
-        
         
         
     </SafeAreaView>
     
-
     )
     
 }
@@ -40,5 +50,29 @@ const style =StyleSheet.create(
         flex:7
     },
 
+    titleBar:{
+        flex:1,
+        justifyContent:'flex-end'
+    },
 
-})
+    title:{
+            fontFamily: commonStyles.fontFamily,
+            fontSize: 50,
+            color: commonStyles.colors.secundary,
+            marginLeft: 20,
+            marginBottom: 20,
+
+    },
+
+    subTitle: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color:commonStyles.colors.secundary,
+        marginLeft:20,
+        marginBottom: 20,
+    },
+
+    }
+
+
+)
